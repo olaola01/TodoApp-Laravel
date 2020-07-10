b1245d0b14f66e08d4d749273f1223c2e9bce02b
olaola01 <olaposi1409@gmail.com>
2020-07-10 18:46:44 +0100

    Todo App

---
+++ resources/views/todos/create.blade.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Laravel
+ * By: Olamiposi
+ * 09/07/2020
+ * 2020
+ **/
+?>
+
+@extends('layout.app')
+
+@section('content')
+
+    <h1 class="text-center my-5">Create Todos</h1>
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card card-default">
+                <div class="card-header">
+                    Create new todo
+                </div>
+                <div class="card-body">
+                    @if ($errors->any())
+                        <div class="alert alert-danger">
+                            <ul class="list-group">
+                                @foreach($errors->all() as $error)
+
+                                <li class="list-group-item">
+                                    {{ $error }}
+                                </li>
+                                @endforeach
+
+                            </ul>
+                        </div>
+                    @endif
+                    <form action="../store-todos" method="POST">
+                        @csrf
+                        <div class="form-group">
+                            <input type="text" class="form-control" placeholder="Name" name="name">
+                        </div>
+                        <div class="form-group">
+                            <textarea name="description" cols="5" row="5"  placeholder="Description" class="form-control"></textarea>
+                        </div>
+
+                    <div class="form-group text-center">
+                        <button type="submit" class="btn btn-success">Create Todo</button>
+                    </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+//Route::get('about', 'AboutController@index');
+
+Route::get('todos', 'TodosController@index');
+Route::get('todos/{todo}', 'TodosController@show');
+Route::get('new-todos', 'TodosController@create');
+Route::post('store-todos', 'TodosController@store');
+Route::get('todos/{todos}/edit', 'TodosController@edit');
+Route::post('todos/{todos}/update-todos', 'TodosController@update');
+Route::get('todos/{todos}/delete', 'TodosController@destroy');
+Route::get('todos/{todo}/complete', 'TodosConntroller@complete');
+
